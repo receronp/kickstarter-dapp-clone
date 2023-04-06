f64546602b7ea1937445bad2e6210f5cfbf8d400
Raul Ceron <receronp@gmail.com>
2023-04-06 11:19:17 -0600

    Update factory contract to get campaign summary.

--- truffle/factory.js
+++ truffle/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   CampaignFactory.abi,
-  "0xD3dc0F6bA2332490ef3144720D9d158Bb9dC5F20"
+  "0xA35198AFbDCb7218e0eA61bFD53DCd0546F3242B"
 );
 
 export default instance;
