31d313a29df8b662508f25caf995957fc6183c69
Raul Ceron <receronp@gmail.com>
2023-04-06 11:58:30 -0600

    Adds Campaign contract request functionality.

---
+++ client/components/RequestRow.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
+import { Button, Table } from "semantic-ui-react";
+import web3 from "../../truffle/web3";
+import Campaign from "../../truffle/Campaign";
+
+const RequestRow = ({ request, id, approversCount, address }) => {
+  const { Row, Cell } = Table;
+  const readyToFinalize = request.approvalCount > approversCount / 2;
+
+  const onApprove = async () => {
+    const campaign = Campaign(address);
+    const accounts = await web3.eth.getAccounts();
+
+    await campaign.methods.approveRequest(id).send({ from: accounts[0] });
+  };
+
+  const onFinalize = async () => {
+    const campaign = Campaign(address);
+    const accounts = await web3.eth.getAccounts();
+
+    await campaign.methods.finalizeRequest(id).send({ from: accounts[0] });
+  };
+
+  return (
+    <Row
+      disabled={request.complete}
+      positive={readyToFinalize && !request.complete}
+    >
+      <Cell>{id}</Cell>
+      <Cell>{request.description}</Cell>
+      <Cell>{web3.utils.fromWei(request.value, "ether")}</Cell>
+      <Cell>{request.recipient}</Cell>
+      <Cell>
+        {request.approvalCount}/{approversCount}
+      </Cell>
+      <Cell>
+        {!request.complete && (
+          <Button color="green" basic onClick={onApprove}>
+            Approve
+          </Button>
+        )}
+      </Cell>
+      <Cell>
+        {!request.complete && (
+          <Button color="teal" basic onClick={onFinalize}>
+            Finalize
+          </Button>
+        )}
+      </Cell>
+    </Row>
+  );
+};
+
+export default RequestRow;
