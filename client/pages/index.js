140b1ee1d70107722afa07d5be48e5630b154d52
Raul Ceron <receronp@gmail.com>
2023-04-06 11:24:20 -0600

    Refactor index to function, adds layout components

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
+import Link from "next/link";
+import { Button, Card } from "semantic-ui-react";
+
+import Layout from "../components/Layout";
 import factory from "../../truffle/factory";
 
-class CampaignIndex extends Component {
-  async componentDidMount() {
-    const campaigns = await factory.methods.getDeployedCampaigns().call();
-    console.log(campaigns);
-  }
+function CampaignIndex({ campaigns }) {
+  const renderCampaigns = () => {
+    const items = campaigns.map((address) => {
+      return {
+        header: address,
+        description: <Link href={`/campaigns/${address}`}>View Campaign</Link>,
+        fluid: true,
+      };
+    });
 
-  render() {
-    return <div>Campaigns Index!</div>;
-  }
+    return <Card.Group items={items} />;
+  };
+
+  return (
+    <Layout>
+      <h3>Open Campaigns</h3>
+      <Link href="/campaigns/new">
+        <Button content="Create Campaign" icon="add" floated="right" primary />
+      </Link>
+      {renderCampaigns()}
+    </Layout>
+  );
 }
+
+CampaignIndex.getInitialProps = async () => {
+  const campaigns = await factory.methods.getDeployedCampaigns().call();
+  return { campaigns };
+};
 
 export default CampaignIndex;
